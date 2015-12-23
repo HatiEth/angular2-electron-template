de86739615d71e31a5cdabe3ba6f2701cf2b60b9
Hati Eth <hati.eth@gmail.com>
2015-12-24 00:53:39 +0100

    update and working injection...

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import {AppComponent} from './app.component';
+import {HeroService} from './hero.service';
 import {bootstrap} from 'angular2/bootstrap';
 
-bootstrap(AppComponent)
+bootstrap(AppComponent, [HeroService])
