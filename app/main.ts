e736fff4224de7d1d3d1cdc169f36a2a757bb426
Hati Eth <hati.eth@gmail.com>
2015-12-23 22:24:48 +0100

    move typings

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,12 @@
-/// <reference path="typings/angular2.d.ts"/>
-
 import 'reflect-metadata';
-import {Component} from 'angular2/core';
-import {bootstrap} from 'angular2/bootstrap'
+import {Component, View} from 'angular2/core';
+import {bootstrap} from 'angular2/bootstrap';
 
 @Component({
   selector: 'app',
   template: '<h1>My first app</h1>'
 })
+
 
 export class AppComponent {
   public title = 'Tour of Heroes';
